4271cb62d3ccfd2d008ea9eb392ab5ab0bccf238
Brunopp <marduk.dk@gmail.com>
2014-03-10 23:32:06 +0100

    FlowLayout added. Children in the FlowLayout are added dynamically (=by code).

--- src/com/example/kodetilkunde/MainActivity.java
+++ src/com/example/kodetilkunde/MainActivity.java
@@ -3,21 +3,50 @@ package com.example.kodetilkunde;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.apmem.tools.layouts.*;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
+		
+		// Get the container holding the categories.
+		LinearLayout container = (LinearLayout) findViewById(R.id.layoutContainer);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
+		// Create 8 categories.
+		for (int i = 0; i < 8; i++) {
+			// Inflate the FlowLayout (=create a flowlayout from an XML file).
+			FlowLayout fl = (FlowLayout) View.inflate(this, R.layout.flow_layout, null);
+			// Find the TextView in the FlowLayout.
+			TextView tv = (TextView) fl.findViewById(R.id.textView);
+			tv.setText("KATEGORI" + i);
+			
+			// Add 10 buttons to the FlowLayout.
+			for (int j = 0; j < 10; j++) {
+				Button b = new Button(this);
+				b.setText("BUTTON" + j);
+				
+				fl.addView(b);
+			}
+			
+			// Add the FlowLayout to the container.
+			container.addView(fl);
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
 }
