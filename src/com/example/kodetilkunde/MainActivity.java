adb67da36944abed9924c2d1128de2241409f56d
Danny Melsen <dannymelsen@gmail.com>
2014-03-17 09:01:13 +0100

    Inserting bricks

--- src/com/example/kodetilkunde/MainActivity.java
+++ src/com/example/kodetilkunde/MainActivity.java
@@ -49,16 +49,148 @@ public class MainActivity extends Activity {
          * Inserting
          * */
         // Inserting Bricks
-        Log.d("Insert: ", "Inserting ..");
+        Log.d("Insert: ", "Inserting Lilla purple...");
         db.createBricks(new Bricks (1, "Æstetik" , "lilla"));
         db.createBricks(new Bricks (2, "Privat" , "lilla"));
         db.createBricks(new Bricks (3, "Inspiration" , "lilla"));
         db.createBricks(new Bricks (4, "Skygge" , "lilla"));
         db.createBricks(new Bricks (5, "Historie" , "lilla"));
         db.createBricks(new Bricks (6, "Støj" , "lilla"));
+        db.createBricks(new Bricks (7, "Modernisme" , "lilla"));
+        db.createBricks(new Bricks (7, "Farver" , "lilla"));
+        db.createBricks(new Bricks (7, "Stilhed" , "lilla"));        
+        db.createBricks(new Bricks (1, "Høj" , "lilla"));
+        db.createBricks(new Bricks (2, "Integreret" , "lilla"));
+        db.createBricks(new Bricks (3, "Trængsel" , "lilla"));
+        db.createBricks(new Bricks (4, "System" , "lilla"));
+        db.createBricks(new Bricks (5, "Ekslusiv" , "lilla"));
+        db.createBricks(new Bricks (6, "Struktur" , "lilla"));
+        db.createBricks(new Bricks (7, "Overflader" , "lilla"));
+        db.createBricks(new Bricks (7, "Rolig" , "lilla"));
+        db.createBricks(new Bricks (7, "Lav" , "lilla"));   
+        db.createBricks(new Bricks (1, "Passiv" , "lilla"));
+        db.createBricks(new Bricks (2, "Terræn" , "lilla"));
+        db.createBricks(new Bricks (3, "Offentlig" , "lilla"));
+        db.createBricks(new Bricks (4, "Intelligent" , "lilla"));
+        db.createBricks(new Bricks (5, "Billig" , "lilla"));
+        db.createBricks(new Bricks (6, "Oplevelse" , "lilla"));
+        db.createBricks(new Bricks (7, "Rytme" , "lilla"));
+        db.createBricks(new Bricks (7, "Udtryk" , "lilla"));
+        db.createBricks(new Bricks (7, "Kaotisk" , "lilla"));
+        db.createBricks(new Bricks (1, "Tyk" , "lilla"));
+        db.createBricks(new Bricks (2, "Aktiv" , "lilla"));
+        db.createBricks(new Bricks (3, "Grønne områder" , "lilla"));
+        db.createBricks(new Bricks (4, "Kold" , "lilla"));
+        db.createBricks(new Bricks (5, "Minimere" , "lilla"));
+        db.createBricks(new Bricks (6, "Vinter" , "lilla"));
+        db.createBricks(new Bricks (7, "Brugerstyring" , "lilla"));
+        db.createBricks(new Bricks (7, "Fortælling" , "lilla"));
+        db.createBricks(new Bricks (7, "Livsstil" , "lilla"));
+        db.createBricks(new Bricks (1, "Symmetri" , "lilla"));
+        db.createBricks(new Bricks (2, "Tynd" , "lilla"));
+        db.createBricks(new Bricks (3, "Stor" , "lilla"));
+        db.createBricks(new Bricks (4, "Nord" , "lilla"));
+        db.createBricks(new Bricks (5, "Varm" , "lilla"));
+        db.createBricks(new Bricks (6, "Fællesskab" , "lilla"));
+        db.createBricks(new Bricks (7, "Sommer" , "lilla"));
+        db.createBricks(new Bricks (7, "Organisk" , "lilla"));
+        db.createBricks(new Bricks (7, "Udsigt" , "lilla"));
+        db.createBricks(new Bricks (1, "Brugere" , "lilla"));
+        db.createBricks(new Bricks (2, "Asymmetri" , "lilla"));
+        db.createBricks(new Bricks (3, "Larm" , "lilla"));
+        db.createBricks(new Bricks (4, "Lille" , "lilla"));
+        db.createBricks(new Bricks (5, "Syd" , "lilla"));
+        db.createBricks(new Bricks (6, "Kontrast" , "lilla"));
+        db.createBricks(new Bricks (7, "Less is more" , "lilla"));
+        db.createBricks(new Bricks (7, "Perception" , "lilla"));
+        db.createBricks(new Bricks (7, "Solen" , "lilla"));
+        db.createBricks(new Bricks (1, "Dagslys" , "lilla"));
+        db.createBricks(new Bricks (2, "Diffus" , "lilla"));
+        db.createBricks(new Bricks (3, "Sundhed" , "lilla"));
+        db.createBricks(new Bricks (4, "Økonomi" , "lilla"));
+        db.createBricks(new Bricks (5, "Fornem" , "lilla"));
+        db.createBricks(new Bricks (6, "xxxxxx" , "lilla"));
+        db.createBricks(new Bricks (7, "Åben" , "lilla"));
+        db.createBricks(new Bricks (7, "Øst" , "lilla"));
+        db.createBricks(new Bricks (7, "Leg" , "lilla"));
+        db.createBricks(new Bricks (1, "Alvorlig" , "lilla"));
+        db.createBricks(new Bricks (2, "Beklædning" , "lilla"));
+        db.createBricks(new Bricks (3, "Regn" , "lilla"));
+        db.createBricks(new Bricks (4, "Tydelig" , "lilla"));
+        db.createBricks(new Bricks (5, "Livskvalitet" , "lilla"));
+        db.createBricks(new Bricks (6, "xxxxxx" , "lilla"));
+        db.createBricks(new Bricks (7, "Nuancer" , "lilla"));
+        db.createBricks(new Bricks (7, "xxxxxx" , "lilla"));
+        db.createBricks(new Bricks (7, "Lukket" , "lilla"));
+        db.createBricks(new Bricks (6, "Vest" , "lilla"));
+        db.createBricks(new Bricks (7, "Humor" , "lilla"));
+        db.createBricks(new Bricks (7, "Højtidelig" , "lilla"));
+        db.createBricks(new Bricks (7, "Lugt" , "lilla"));	
+        db.createBricks(new Bricks (6, "Komfort" , "lilla"));
+        db.createBricks(new Bricks (7, "Rytme" , "lilla"));
+        db.createBricks(new Bricks (7, "xxxxxx" , "lilla"));
+        db.createBricks(new Bricks (7, "xxxxxx" , "lilla"));	
+        db.createBricks(new Bricks (6, "Værdiskabelse" , "lilla"));
+        db.createBricks(new Bricks (7, "xxxxxx" , "lilla"));
+        db.createBricks(new Bricks (7, "Mindmapping" , "lilla"));
+        db.createBricks(new Bricks (7, "Natur" , "lilla"));
+        db.createBricks(new Bricks (7, "Mødested" , "lilla"));
+        db.createBricks(new Bricks (7, "Sikker" , "lilla"));	
+        db.createBricks(new Bricks (7, "Aroma" , "lilla"));
+        db.createBricks(new Bricks (7, "Akustik" , "lilla"));	
+        db.createBricks(new Bricks (7, "Naturlig" , "lilla"));
+        db.createBricks(new Bricks (7, "xxxxxx" , "lilla"));	
+        db.createBricks(new Bricks (7, "xxxxxx" , "lilla"));
+        db.createBricks(new Bricks (7, "Tryghed" , "lilla"));
+        db.createBricks(new Bricks (7, "xxxxxx" , "lilla"));
+        db.createBricks(new Bricks (7, "Benspænd" , "lilla"));	
+        db.createBricks(new Bricks (7, "Introvert" , "lilla"));
+        db.createBricks(new Bricks (7, "Træk" , "lilla"));	
+        db.createBricks(new Bricks (7, "Skæv" , "lilla"));
+        db.createBricks(new Bricks (7, "Principper" , "lilla"));	
+        db.createBricks(new Bricks (7, "funktion" , "lilla"));
+        db.createBricks(new Bricks (7, "Oprørsk" , "lilla"));
+        db.createBricks(new Bricks (7, "xxxxxx" , "lilla"));	
+        db.createBricks(new Bricks (7, "xxxxxx" , "lilla"));
+        db.createBricks(new Bricks (7, "Akustik" , "lilla"));	
+        db.createBricks(new Bricks (7, "xxxxxx" , "lilla"));
+        db.createBricks(new Bricks (7, "Litteratur" , "lilla"));	
+        db.createBricks(new Bricks (7, "Ekstrovert" , "lilla"));	
+        db.createBricks(new Bricks (7, "Orientering" , "lilla"));
+        db.createBricks(new Bricks (7, "ankomst" , "lilla"));	
+        db.createBricks(new Bricks (7, "geometri" , "lilla"));
+        db.createBricks(new Bricks (7, "" , "lilla"));	
+        db.createBricks(new Bricks (7, "" , "lilla"));	
+        db.createBricks(new Bricks (7, "" , "lilla"));
+        db.createBricks(new Bricks (7, "" , "lilla"));	
+        db.createBricks(new Bricks (7, "" , "lilla"));
+        db.createBricks(new Bricks (7, "" , "lilla"));	
 
         
-        // insertings Categories
+		 /**
+         * Inserting
+         * */
+        // Inserting Bricks
+        Log.d("Insert: ", "Inserting Lilla Green...");
+        
+        db.createBricks(new Bricks (7, "" , "Green"));	
+
+
+		 /**
+         * Inserting
+         * */
+        // Inserting Bricks
+        Log.d("Insert: ", "Inserting Lilla Yellow...");
+        
+        db.createBricks(new Bricks (7, "" , "Yellow"));	
+
+        
+        
+		 /**
+         * Inserting
+         * */
+        // Inserting Categories
+        Log.d("Insert: ", "Inserting Categories...");
         db.createCategory(new Category (1, "Arkitektur" , "Hvid"));
         db.createCategory(new Category (2, "Brugere/Funktion" , "Sand"));
         db.createCategory(new Category (3, "Industralisering" , "Lyserblå"));
